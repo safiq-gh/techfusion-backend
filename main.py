75666328ac55493fa88532a7ca2af0bee85a64de
safiq-gh <230081601045@crescent.education>
2026-02-28 19:13:41 +0530

    Open CORS for Testing

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://techfusion-ui.vercel.app", "https://techfusionfrontend.vercel.app"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
